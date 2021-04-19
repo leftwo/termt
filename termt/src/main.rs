4683b0e4376c430efbaf82df7bd8a760ef4182a7
alan <leftwo@gmail.com>
2021-04-19 10:53:02 -0700

    Setup alt screen and game loop

--- termt/src/main.rs
+++ termt/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{event, terminal, ExecutableCommand};
+use std::error::Error;
+use std::time::{Duration};
+use std::{io, thread};
+
+// Inspired (and some initial code) from https://github.com/CleanCut/invaders
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Game loop
+    'gameloop: loop {
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Updates
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Cleanup
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
