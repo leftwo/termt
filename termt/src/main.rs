00d6d51cd6a7add553ef348805e60bd714b23c7e
alan <leftwo@gmail.com>
2021-04-19 11:02:33 -0700

    Ran cargo fmt to address PR comments

--- termt/src/main.rs
+++ termt/src/main.rs
@@ -3,13 +3,12 @@ use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::{event, terminal, ExecutableCommand};
 use std::error::Error;
-use std::time::{Duration};
+use std::time::Duration;
 use std::{io, thread};
 
 // Inspired (and some initial code) from https://github.com/CleanCut/invaders
 
 fn main() -> Result<(), Box<dyn Error>> {
-
     // Terminal
     let mut stdout = io::stdout();
     terminal::enable_raw_mode()?;
